df98bab3be4282c9c589c14a99b1fb19c0f9f83b
ralf <rb@egroupware.org>
2025-08-15 09:27:15 +0200

    changes for kdots

--- js/app.ts
+++ js/app.ts
@@ -10,18 +10,34 @@
 
 import {app} from "../../api/js/jsapi/egw_global";
 import { EgwApp } from '../../api/js/jsapi/egw_app';
+import type {Et2ButtonToggle} from "../../api/js/etemplate/Et2Button/Et2ButtonToggle";
+import type {Et2Select} from "../../api/js/etemplate/Et2Select/Et2Select";
+import type {et2_nextmatch} from "../../api/js/etemplate/et2_extension_nextmatch";
 
 class DeveloperApp extends EgwApp
 {
 	// app name
 	readonly appname = 'developer';
 
+	protected nm : et2_nextmatch;
 	/**
 	 * app js initialization stage
 	 */
 	constructor(appname: string)
 	{
 		super(appname);
+
+		this.nmFilterChange = this.nmFilterChange.bind(this);
+	}
+
+	destroy(_app)
+	{
+		super.destroy(_app);
+
+		if (this.nm && this.nm.getDOMNode())
+		{
+			this.nm.getDOMNode().removeEventListener('et2-filter', this.nmFilterChange);
+		}
 	}
 
 	/**
@@ -34,7 +50,58 @@ class DeveloperApp extends EgwApp
 	{
 		// call parent
 		super.et2_ready.apply(this, arguments);
+
+		switch(name)
+		{
+			case "developer.translations.index":
+				this.nm = this.et2.getWidgetById('nm');
+				this.nm.getDOMNode().addEventListener('et2-filter', this.nmFilterChange);
+				const untranslated_toggle = this.et2.getWidgetById('nm[untranslated]');
+				if (untranslated_toggle) {
+					window.setTimeout(() => {
+						untranslated_toggle.value = this.et2.getArrayMgr('content').getEntry('nm[filter2]') === 'untranslated';
+					}, 100);
+				}
+				break;
+		}
+	}
+
+	/**
+	 * Keep untranslated toggle and app-selector in sync with NM / filter thingy
+	 *
+	 * @param _ev : Event
+	 */
+	nmFilterChange(_ev : Event)
+	{
+		const app_change = this.et2.getWidgetById('nm[cat_id]');
+		if (app_change && app_change.value != _ev.detail.activeFilters.cat_id)
+		{
+			app_change.value = _ev.detail.activeFilters.cat_id;
+		}
+		const untranslated_toggle = this.et2.getWidgetById('nm[untranslated]');
+		if (untranslated_toggle && (untranslated_toggle.value === 'untranslated') != (_ev.detail.activeFilters.filter2 === 'untranslated')) {
+			untranslated_toggle.value = _ev.detail.activeFilters.filter2 === 'untranslated';
+		}
+	}
+
+	/**
+	 * Show only untranslated has been clicked
+	 */
+	toggleUntranslated(_ev : Event, _widget : Et2ButtonToggle)
+	{
+		this.nm && this.nm.applyFilters({filter2: _widget.value ? 'untranslated' : ''});
+	}
+
+	/**
+	 * Propagate app-selection to NM and filter thingy
+	 *
+	 * @param _ev
+	 * @param _widget
+	 */
+	changeApp(_ev : Event, _widget : Et2Select)
+	{
+		this.nm && this.nm.applyFilters({cat_id: _widget.value});
 	}
 }
 
-app.classes.example = DeveloperApp;
+app.classes.developer = DeveloperApp;
